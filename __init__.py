1cb6134082d64372254f6c78bf0e1b562eddf3ed
Joe Burmeister <joe.burmeister@devtank.co.uk>
2021-08-03 14:28:16 +0100

    Update how Py2/Py3 support is done.

--- __init__.py
+++ __init__.py
@@ -3,24 +3,21 @@
 #
 # LoTI firmware wrapper
 #
+from __future__ import print_function, absolute_import
 
 import sys
 
 VERSION = 1.0
 
-if sys.version_info.major == 3:
-    ##
-    # @brief Publically exportable modules
-    #
-    __all__ = [
-        "binding",
-    ]
+##
+# @brief Publically exportable modules
+#
+__all__ = [
+    "binding",
+]
 
-    ##
-    # @brief Import all variables in these modules into other namespaces
-    #
-    from .binding import io_board_py_t, uart_t, io_t, adc_t, pps_t
-    from .binding import debug_print as io_debug_print, set_debug_print as io_set_debug_print, get_debug_print as io_get_debug_print
-else:
-    from binding import io_board_py_t, uart_t, io_t, adc_t, pps_t
-    from binding import debug_print as io_debug_print, set_debug_print as io_set_debug_print, get_debug_print as io_get_debug_print
+##
+# @brief Import all variables in these modules into other namespaces
+#
+from .binding import io_board_py_t, uart_t, io_t, adc_t, pps_t
+from .binding import debug_print as io_debug_print, set_debug_print as io_set_debug_print, get_debug_print as io_get_debug_print
